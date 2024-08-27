25ebe5cd0932b1e8b156d5c6ed7bc41781928dc3
Wyatt Joseph Knepp <wknepp@hydra14.eecs.utk.edu>
2024-08-27 14:33:41 -0400

    Initial commit

--- lib_info.cpp
+++ lib_info.cpp
@@ -1,61 +1,66 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <map>
 #include <fstream>
 #include <sstream>
 using namespace std;
 
-
-struct Song {       // holds an individual song
+struct Track
+{ // holds an individual song
     string title;
-    int time;
+    string artist;
+    string album;
+    int songTrackNumber;
+    int timeSeconds;
 };
 
-
-struct Album {      // each album holds a container of its associated songs
-    map<int, Song> songs;
+struct Album
+{ // each album holds a container of its associated songs
+    map<int, Track> songs;
     string name;
     int time;
     int numSongs;
 };
 
-
-struct Artist {     // each artist holds a container of its associated albums
+struct Artist
+{ // each artist holds a container of its associated albums
     map<string, Album> albums;
     string name;
     int time;
     int numSongs;
 };
 
+int convertToSeconds(string timeIn); // convert time in [min:sec] format to secs
 
-int convertToSeconds (string timeIn);   // convert time in [min:sec] format to secs
+string convertToMinutesAndSeconds(int timeIn); // converts time in secs back to [min:sec] format
 
+void underscoreToSpace(string &input); // converts all underscores in an input to spaces
 
-string convertToMinutesAndSeconds (int timeIn);     // converts time in secs back to [min:sec] format
-
-
-void underscoreToSpace (string &input);     // converts all underscores in an input to spaces
-
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     // incorrect command line error
-    if (argc != 2) {
+    if (argc != 2)
+    {
         cerr << "Invalid number of command line args. Format is ./solution [filename]\n";
         return 1;
     }
 
     string filename = argv[1];
-    string line;                // holds a given line of the text file
+    string line; // holds a given line of the text file
     ifstream fin(filename);
 
     // incorrect filename error
-    if (!fin.is_open()) {
+    if (!fin.is_open())
+    {
         cerr << "Invalid filename. Please try again.\n";
         return 1;
     }
 
     istringstream sin;
+
+    vector<string> songInputStrings;
 
     // the following variables are for reading in with sstream
     string songName;
@@ -65,63 +70,66 @@ int main(int argc, char **argv) {
     string songGenre;
     int songTrack;
 
-    while (getline(fin, line)) {
+    while (getline(fin, line))
+    {
         sin.str(line);
 
         sin >> songName >> songLen >> songArtist >> songAlbum >> songGenre >> songTrack;
 
-        int songLenInt = convertToSeconds(songLen);     // gives us a time in secs for the album map
+        int songLenInt = convertToSeconds(songLen); // gives us a time in secs for the album map
 
         underscoreToSpace(songName);
         underscoreToSpace(songArtist);
         underscoreToSpace(songAlbum);
         underscoreToSpace(songGenre);
 
-        /*
-
-
-        This section here needs to be done. 
-        
-        Maps on each level need to be formed.
-        
-        
-        */
-
         sin.clear();
+
+        trackToAdd = createTrack(songName, songArtist, songAlbum, songGenre);
     }
 
     /*
-    
+
+
+    This section here needs to be done.
+
+    Maps on each level need to be formed.
+
+
+    */
+
+    /*
+
     We need to do output here
-    
+
     */
 
     return 0;
 }
 
-
-int convertToSeconds (string timeIn) {
+int convertToSeconds(string timeIn)
+{
     int outputTime;
     string mins = "";
     string secs = "";
-    bool isMin = true;  // this differentiates between the left and right of the colon
-    
+    bool isMin = true; // this differentiates between the left and right of the colon
+
     for (size_t i = 0; i < timeIn.length(); i++)
 
-        if (timeIn[i] != ':' && isMin == true)      // left of the colon
+        if (timeIn[i] != ':' && isMin == true) // left of the colon
             mins += timeIn[i];
-        else if (timeIn[i] != ':' && isMin == false)    // right of the colon
+        else if (timeIn[i] != ':' && isMin == false) // right of the colon
             secs += timeIn[i];
-        else                                        // is the colon
+        else // is the colon
             isMin = false;
 
-    outputTime = 60 * stoi(mins) + stoi(secs);  // convert mins to secs and add to existing secs
+    outputTime = 60 * stoi(mins) + stoi(secs); // convert mins to secs and add to existing secs
 
     return outputTime;
 }
 
-
-string convertToMinutesAndSeconds (int timeIn) {
+string convertToMinutesAndSeconds(int timeIn)
+{
     string outputTime;
     string min, sec;
 
@@ -133,11 +141,16 @@ string convertToMinutesAndSeconds (int timeIn) {
     return outputTime;
 }
 
-
-void underscoreToSpace (string &input) {
+void underscoreToSpace(string &input)
+{
     int len = input.length();
 
     for (int i = 0; i < len; i++)
         if (input[i] == '_')
             input[i] = ' ';
+}
+
+Track createTrack(songName, songArtist, songAlbum, songGenre)
+{
+    Track track = {} return
 }
